c6e74f1fcb316c9f5bd0053af245a54c5e75585a
Mariusz Blajet <mariuszblajet@gmail.com>
2019-12-08 12:49:59 +0100

    fix(LandingPage): fixing hamburger menu

--- script.js
+++ script.js
@@ -1,49 +1,86 @@
-//Assignment #1 - hamburder menu
-
-// depends on width - hide or show navigation
+//Assignment #1 - hamburger menu
+const header = document.querySelector('.page-header');
 const navigation = document.querySelector('.page-navigation');
 const hamburgerIcon = document.querySelector('.mobile-button');
 const pageHeader = document.querySelector('.page-navigation__items');
 const pageNavigationLink = document.querySelector('.page-navigation__items');
 
-function colapseMenuOnLoad (){
-    navigation.classList.add('page-navigation--hidden')
+function showMenu(){
+    navigation.classList.remove('page-navigation--hidden');
+    header.classList.add('page-header--expand');
 }
 
+function hideMenu() {
+    navigation.classList.add('page-navigation--hidden');
+    header.classList.remove('page-header--expand');
+}
+
+function showMenuItems() {
+    pageHeader.classList.add('page-navigation__items--expand');
+}
+
+function hideMenuItems(){
+    pageHeader.classList.remove('page-navigation__items--expand');
+}
+
+function addCloseMenuIcon(){
+    hamburgerIcon.classList.add('mobile-button-close');
+}
+
+function removeCloseMenuIcon(){
+    hamburgerIcon.classList.remove('mobile-button-close');  
+}
+
+function addHamburgerMenuIcon() {
+    hamburgerIcon.classList.add('mobile-button-hamburger');
+}
+
+function removeHamburgerMenuIcon(){
+    hamburgerIcon.classList.remove('mobile-button-hamburger');
+}
+
+// colapse menu after page reloads
+function colapseMenuOnLoad (){
+    hideMenu();
+    removeCloseMenuIcon();
+    addHamburgerMenuIcon();
+}
 window.onload = colapseMenuOnLoad;
 
+// switching between hamburger menu states
 hamburgerIcon.addEventListener('click', () => {
     if(navigation.className === 'page-navigation') {
-        hamburgerIcon.classList.remove('mobile-button-close');
-        hamburgerIcon.classList.add('mobile-button-hamburger');
-        pageHeader.classList.remove('page-navigation__items--expand');
-        navigation.classList.add('page-navigation--hidden');
-    } else if (!navigation.className !== 'page-navigation') {
-        hamburgerIcon.classList.remove('mobile-button-hamburger');
-        hamburgerIcon.classList.add('mobile-button-close');
-        navigation.classList.remove('page-navigation--hidden');
-        pageHeader.classList.add('page-navigation__items--expand');
+        hideMenuItems();
+        hideMenu();
+        removeCloseMenuIcon();
+        addHamburgerMenuIcon();
+    } else if (navigation.className !== 'page-navigation') {
+        removeHamburgerMenuIcon();
+        addCloseMenuIcon();
+        showMenu();
+        showMenuItems();
     }
 })
 
-//colapse manu after selecting anchor link
+//colapse menu after selecting anchor link
 pageNavigationLink.addEventListener('click', (e)=> {
     if(e.target.tagName === 'A') {
-        navigation.classList.add('page-navigation--hidden')
-        hamburgerIcon.classList.remove('mobile-button-close');
-        hamburgerIcon.classList.add('mobile-button-hamburger');
+        hideMenu();
+        removeCloseMenuIcon();
+        addHamburgerMenuIcon();
     }
 })
 
+// depends on width - hide or show navigation 
+// between 320-375px menu should be visible
 function hideOrShowNavigation () {
     if (window.innerWidth <= 375) {
-        console.log(window.innerWidth);
-        navigation.classList.add('page-navigation--hidden')
+        hideMenu();
+        removeCloseMenuIcon();
+        addHamburgerMenuIcon()
     }
 }
-
 window.onresize = hideOrShowNavigation;
-
 
 // Assignment #2
 const buyATicketButton = document.querySelector('.concerts');
