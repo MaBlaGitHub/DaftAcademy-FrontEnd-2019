014c4290a83737c580142fb46327029fcf21915e
Mariusz Blajet <mariuszblajet@gmail.com>
2019-12-14 20:20:33 +0100

    feat(LandingPage): animate hamburger menu and gallery

--- script.js
+++ script.js
@@ -5,47 +5,56 @@ const hamburgerIcon = document.querySelector('.mobile-button');
 const pageHeader = document.querySelector('.page-navigation__items');
 const pageNavigationLink = document.querySelector('.page-navigation__items');
 
-function showMenu(){
-    navigation.classList.remove('page-navigation--hidden');
-    header.classList.add('page-header--expand');
+// change state from hamburger to X
+hamburgerIcon.addEventListener('click', () => {
+    hamburgerIcon.classList.toggle('modify')
+})
+
+const showMenu = () => {
+    navigation.classList.toggle('page-navigation--hidden');
+    header.classList.toggle('page-header--expand');
 }
 
-function hideMenu() {
+const hideMenu = () => {
     navigation.classList.add('page-navigation--hidden');
     header.classList.remove('page-header--expand');
 }
 
-function showMenuItems() {
+const showMenuItems = () => {
     pageHeader.classList.add('page-navigation__items--expand');
 }
 
-function hideMenuItems(){
+const hideMenuItems = () => {
     pageHeader.classList.remove('page-navigation__items--expand');
 }
 
-function addCloseMenuIcon(){
+const addCloseMenuIcon = () => {
     hamburgerIcon.classList.add('mobile-button-close');
 }
 
-function removeCloseMenuIcon(){
+const removeCloseMenuIcon = () => {
     hamburgerIcon.classList.remove('mobile-button-close');  
 }
 
-function addHamburgerMenuIcon() {
+const addHamburgerMenuIcon = () => {
     hamburgerIcon.classList.add('mobile-button-hamburger');
 }
 
-function removeHamburgerMenuIcon(){
+const removeHamburgerMenuIcon = () => {
     hamburgerIcon.classList.remove('mobile-button-hamburger');
 }
 
 // colapse menu after page reloads
-function colapseMenuOnLoad (){
+const colapseMenuOnLoad = () => {
     hideMenu();
     removeCloseMenuIcon();
     addHamburgerMenuIcon();
 }
-window.onload = colapseMenuOnLoad;
+// window.onload = 
+
+window.addEventListener('load', () => {
+    colapseMenuOnLoad();
+})
 
 // switching between hamburger menu states
 hamburgerIcon.addEventListener('click', () => {
@@ -63,38 +72,24 @@ hamburgerIcon.addEventListener('click', () => {
 })
 
 //colapse menu after selecting anchor link
-pageNavigationLink.addEventListener('click', (e)=> {
+pageNavigationLink.addEventListener('click', (e) => {
     if(e.target.tagName === 'A') {
         hideMenu();
-        removeCloseMenuIcon();
-        addHamburgerMenuIcon();
+        hamburgerIcon.classList.toggle('modify');
     }
 })
-
-// depends on width - hide or show navigation 
-// between 320-375px menu should be visible
-function hideOrShowNavigation () {
-    if (window.innerWidth <= 375) {
-        hideMenu();
-        removeCloseMenuIcon();
-        addHamburgerMenuIcon()
-    }
-}
-window.onresize = hideOrShowNavigation;
 
 // Assignment #2
-const buyATicketButton = document.querySelector('.concerts');
+const buyATicketButton = document.querySelectorAll('.concerts__info > button');
 
-buyATicketButton.addEventListener('click', (e) => {
-    if (e.target.className === 'button') {
-        const button = e.target.parentElement;
-        button.children[2].remove()
-        const textElement = document.createElement('p');
-        textElement.classList.add('button--success')
-        textElement.textContent = 'have fun!';
-        button.appendChild(textElement);
-    }
-})
+buyATicketButton.forEach(btn => {
+    btn.addEventListener('click', () => {
+        let textElement = document.createElement('p');
+        textElement.textContent = 'Have fun!';
+        textElement.classList.add('button--success');
+        btn.replaceWith(textElement);
+    });
+});
 
 // Assignment #3
 const form = document.querySelector('.form')
